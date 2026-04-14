e2a24dd9699cc74ebb8f4ea5097b7b9ff86f330d
Laxmi Bandi <laxmibandi.cs23@bmsce.ac.in>
2026-04-14 11:11:51 +0530

    Initial commit - Dockerized FinanceGuard

---
+++ server/index.js
@@ -0,0 +1,62 @@
+import express from "express";
+// import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import helmet from "helmet";
+import morgan from "morgan";
+import kpiRoutes from "./routes/kpi.js";
+import productRoutes from "./routes/product.js";
+import transactionRoutes from "./routes/transaction.js";
+import KPI from "./models/KPI.js";
+import Product from "./models/Product.js";
+import Transaction from "./models/Transaction.js";
+import { kpis, products, transactions } from "./data/data.js";
+
+//app.use(
+//    cors({
+//        origin: [""],
+//        methods: ["POST", "GET", "DELETE"],
+//        credentials: true,
+//    })
+//);
+
+/* CONFIGURATIONS */
+/* CONFIGURATIONS */
+dotenv.config();
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(cors());
+
+app.use("/kpi", kpiRoutes);
+app.use("/product", productRoutes);
+app.use("/transaction", transactionRoutes);
+
+/* PORT FIX ✅ */
+const PORT = process.env.PORT || 9000;
+
+/* MONGOOSE SETUP */
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(async () => {
+  console.log("✅ MongoDB Connected");
+
+  if ((await KPI.countDocuments()) === 0) {
+    await KPI.insertMany(kpis);
+  }
+
+  if ((await Product.countDocuments()) === 0) {
+    await Product.insertMany(products);
+  }
+
+  if ((await Transaction.countDocuments()) === 0) {
+    await Transaction.insertMany(transactions);
+  }
+
+  app.listen(PORT, () => console.log(`🚀 Server running on ${PORT}`));
+})
